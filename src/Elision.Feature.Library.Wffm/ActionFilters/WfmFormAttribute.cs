7cde2f2d8473c0b92bf6aab4450ffed69761d4b9
Jeffrey <jrondeau@degdigital.com>
2017-05-05 08:50:56 -0500

    Create form submitter

---
+++ src/Elision.Feature.Library.Wffm/ActionFilters/WfmFormAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Elision.Feature.Library.Wffm.ActionFilters
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public class WfmFormAttribute : Attribute
+	{
+		public string FormName { get; private set; }
+
+		public WfmFormAttribute(string formName)
+		{
+			FormName = formName;
+		}
+	}
+}
---
+++ src/Elision.Feature.Library.Wffm/Models/WfmFormSubmitResults.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Elision.Feature.Library.Wffm.Models
+{
+	public class WfmFormSubmitResults
+	{
+		public WfmFormSubmitStatus Status { get; set; }
+		public IEnumerable<string> Messages { get; set; }
+		public WfmSuccessAction SuccessAction { get; set; }
+		public string SuccessMessage { get; set; }
+		public string SuccessRedirect { get; set; }
+	}
+
+	public enum WfmFormSubmitStatus
+	{
+		Unknown,
+		Success,
+		Failure
+	}
+
+	public enum WfmSuccessAction
+	{
+		Redirect,
+		Message
+	}
+}
